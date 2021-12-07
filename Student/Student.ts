1dbb41d12ed629e310d7dd8d6ad4ca621ad77124
Eung gi Kim <rubie3374@naver.com>
2021-12-07 17:21:34 +0900

    getter, setter 구현

--- Student/Student.ts
+++ Student/Student.ts
@@ -1,11 +1,11 @@
-interface studentInterface {
+export interface studentInterface {
     className : string,
     studentNumber : number
 }
 
 export class Student{
-    private readonly className : string;
-    private readonly studentNumber : number
+    private className : string;
+    private studentNumber : number
     constructor(className : string,studentNumber : number) {
         this.className = className;
         this.studentNumber = studentNumber;
@@ -13,6 +13,12 @@ export class Student{
 
     get getStudent(){
         return {className : this.className, studentNumber : this.studentNumber}
+    }
+
+    set setStudent(value : studentInterface){
+        const {className,studentNumber} = value
+        this.className = className;
+        this.studentNumber = studentNumber
     }
 }
 
--- index.ts
+++ index.ts
@@ -2,4 +2,8 @@ import {Student} from "./Student/Student"
 
 let student1 = new Student("국문학과",123123)
 
+let student2 = new Student("실용음악과",132312)
+
+student2.setStudent = {className : "test",studentNumber : 123123}
 console.log(student1.getStudent)
+console.log(student2.getStudent)
